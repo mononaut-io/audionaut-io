33d377b343a6c943aed3eb3f63a46534a1d4fec7
Sam Ellis <samwilliamellis@gmail.com>
2022-06-03 19:58:34 +0100

    feat: create reusable `modal.component` and `modal.service`
    The `modal.component` utilises generic boilerplate code for building
    reusable modal components. The modal service uses an RxJs
    BehaviourSubject to determine if the component is active (hence,
    deciding if the component should be visible or not).

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IModal } from '@interfaces/modal';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,19 @@ import { Injectable } from '@angular/core';
 export class ModalService {
 
   constructor() { }
+
+  private modal$ = new BehaviorSubject<IModal>({ active: false });
+
+  open() {
+    this.modal$.next({ active: true });
+  }
+
+  close() {
+    this.modal$.next({ active: false });
+  }
+
+  watch(): Observable<IModal> {
+    return this.modal$.asObservable();
+  }
+
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalService } from '@services/modal.service';
+import { IModal } from '@interfaces/modal';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
 })
 export class ModalComponent implements OnInit {
+  modal$: Observable<IModal> = new Observable();
 
-  constructor() { }
+  constructor(private _modalService: ModalService) { }
 
   ngOnInit(): void {
+    this.modal$ = this._modalService.watch();
+  }
+
+  open(): void {
+    this._modalService.open();
+  }
+
+  close(): void {
+    this._modalService.close();
   }
 
 }
