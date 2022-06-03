c5ef92fb53df184d04addc679e79a30a37f21469
Sam Ellis <samwilliamellis@gmail.com>
2022-06-03 23:21:46 +0100

    feat: add fade-in and fade-out `modal.component` animations

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { ModalService } from '@services/modal.service';
 import { IModal } from '@interfaces/modal';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
+  animations: [
+    trigger('fade-in-out', [
+      state('open', style({
+        opacity: 1,
+        visibility: 'visible',
+      })),
+      state('closed', style({
+        opacity: 0,
+        visibility: 'hidden',
+      })),
+      transition("closed => open", [animate("200ms ease-out")]),
+      transition("open => closed", [animate("150ms ease-in")]),
+    ]),
+  ],
 })
 export class ModalComponent implements OnInit {
   modal$: Observable<IModal> = new Observable();
