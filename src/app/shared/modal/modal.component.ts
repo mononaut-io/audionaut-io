d29c4986d46ca20a2e360cc60026e4406e170bcd
Sam Ellis <samwilliamellis@gmail.com>
2022-06-04 12:01:41 +0100

    update: add `scale` and `blur` transitions to `modal.component`
    This commit adds additional child animations to the `modal.component`
    UI. Opening the modal triggers a slight scale transition on the modal
    content, as well as a blur transition on the modal backdrop.

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, query, animateChild, group } from '@angular/animations';
 import { ModalService } from '@services/modal.service';
 import { IModal } from '@interfaces/modal';
 
@@ -8,18 +8,44 @@ import { IModal } from '@interfaces/modal';
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   animations: [
+
     trigger('fade-in-out', [
       state('open', style({
         opacity: 1,
         visibility: 'visible',
+        'backdrop-filter': 'blur(4px)',
       })),
       state('closed', style({
         opacity: 0,
         visibility: 'hidden',
       })),
-      transition("closed => open", [animate("200ms ease-out")]),
-      transition("open => closed", [animate("150ms ease-in")]),
+      transition("closed => open", [
+        group([
+          animate("100ms ease-out"),
+          query('@*', animateChild()),
+        ])
+      ]),
+      transition("open => closed", [
+        group([
+          query('@*', animateChild()),
+          animate("100ms ease-in"),
+        ])
+      ]),
     ]),
+
+    trigger('scale-in-out', [
+      state('open', style({
+        opacity: 1,
+        transform: 'scale(1)',
+      })),
+      state('closed', style({
+        opacity: 0,
+        transform: 'scale(0.97)',
+      })),
+      transition("closed => open", [animate("100ms ease-out")]),
+      transition("open => closed", [animate("100ms ease-in")]),
+    ]),
+
   ],
 })
 export class ModalComponent implements OnInit {
