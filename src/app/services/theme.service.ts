864000b2d4dcf0d00f0585cffad7518ff5c47633
Sam Ellis <samwilliamellis@gmail.com>
2022-06-06 21:40:47 +0100

    feat: add theme-switching functionality
    At the moment, the theme will default to light-mode. In the future,
    the theme will initially default to the user's Operating System
    preference. Manually toggling the theme can be done through the
    `toggle()` service method.

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { LocalStorageService } from '@services/local-storage.service';
+import { ITheme } from '@interfaces/theme';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
 
-  constructor() { }
+  private _localVarName: string = 'theme';
+  private default: ITheme = { dark: false };
+  theme$: BehaviorSubject<ITheme> = new BehaviorSubject<ITheme>(this.default);
+
+  constructor(private _localStorageService: LocalStorageService) {
+    this.initialise();
+    this.theme$.subscribe( theme => {
+      const _classList = document.getElementsByTagName('html').item(0)?.classList;
+      this._localStorageService.set(this._localVarName, theme);
+      theme.dark ? _classList?.add('dark') : _classList?.remove('dark');
+    });
+  }
+
+  private initialise(): void {
+    if (this._localStorageService.get(this._localVarName) === null) {
+      this._localStorageService.set(this._localVarName, this.default);
+    }
+
+    this.theme$.next(
+      Object(this._localStorageService.get(this._localVarName))
+    );
+  }
+
+  toggle(): void {
+    this.theme$.next({ dark: !this.theme$.value.dark });
+  }
+
 }
