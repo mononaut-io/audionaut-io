19048ed50d40a5dd80fa6a9323ed8ca26acf5605
Sam Ellis <samwilliamellis@gmail.com>
2022-06-08 09:44:08 +0100

    update: disable angular animations
    Angular animations are not working on Safari, and from searching online
    I can't find the solution to the issue at this moment. This issue causes
    `modal.component` to become invisible and not able to the toggled on the
    Safari browser.

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,52 +1,12 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { Observable } from 'rxjs';
-import { animate, state, style, transition, trigger, query, animateChild, group } from '@angular/animations';
 import { ModalService } from '@services/modal.service';
 import { IModal } from '@interfaces/modal';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  animations: [
-
-    trigger('fade-in-out', [
-      state('open', style({
-        opacity: 1,
-        visibility: 'visible',
-        'backdrop-filter': 'blur(4px)',
-      })),
-      state('closed', style({
-        opacity: 0,
-        visibility: 'hidden',
-      })),
-      transition("closed => open", [
-        group([
-          animate("100ms ease-out"),
-          query('@*', animateChild()),
-        ])
-      ]),
-      transition("open => closed", [
-        group([
-          query('@*', animateChild()),
-          animate("100ms ease-in"),
-        ])
-      ]),
-    ]),
-
-    trigger('scale-in-out', [
-      state('open', style({
-        opacity: 1,
-        transform: 'scale(1)',
-      })),
-      state('closed', style({
-        opacity: 0,
-        transform: 'scale(0.97)',
-      })),
-      transition("closed => open", [animate("100ms ease-out")]),
-      transition("open => closed", [animate("100ms ease-in")]),
-    ]),
-
-  ],
+  animations: [],
 })
 export class ModalComponent implements OnInit {
   modal$: Observable<IModal> = new Observable();
