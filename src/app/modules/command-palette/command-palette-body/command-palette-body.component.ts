697635ede64c41a2601752293fde94ee5a96299a
Sam Ellis <samwilliamellis@gmail.com>
2022-06-16 12:20:02 +0100

    feat: add dynamic search functionality to `command-palette.component`
    This commit adds basic conditional matching on the input search tag in
    `command-palette.component` so it can be used to conditionally render
    HTML elements.
    
    At the moment, when the search box is empty, a placeholder for user
    history and user favourites is shown (this will be developed in later
    commits). When the user starts typing into the input box, HTML is
    rendered showing the `resultsFound` HTML template.

--- src/app/modules/command-palette/command-palette-body/command-palette-body.component.ts
+++ src/app/modules/command-palette/command-palette-body/command-palette-body.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SearchService } from '@services/search.service';
 
 @Component({
   selector: 'app-cp-body',
   templateUrl: './command-palette-body.component.html',
 })
 export class CommandPaletteBodyComponent implements OnInit {
+  search$: Observable<string> = new Observable();
 
-  constructor() { }
+  constructor(private _searchService: SearchService) {
+    this.search$ = this._searchService.watch();
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private search$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor() { }
+
+  set(value: string): void {
+    this.search$.next(value);
+  }
+
+  reset(): void {
+    this.search$.next('');
+  }
+
+  watch(): Observable<string> {
+    return this.search$.asObservable();
+  }
+
+  complete(): void {
+    this.search$.complete();
+  }
+
 }
